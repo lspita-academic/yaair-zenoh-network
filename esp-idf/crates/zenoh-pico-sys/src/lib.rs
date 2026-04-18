c0c3e606f6ea37229a4fb66185d12a4098005d46
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-04-18 18:43:21 +0200

    zp: splitted sys and wrapper crate

---
+++ esp-idf/crates/zenoh-pico-sys/src/lib.rs
@@ -0,0 +1,3 @@
+#![allow(non_upper_case_globals)]
+
+pub use esp_idf_svc::sys::zenoh_pico::*;
--- esp-idf/crates/zenoh-pico/src/lib.rs
+++ esp-idf/crates/zenoh-pico/src/lib.rs
@@ -1,5 +1,3 @@
-#![allow(non_upper_case_globals)]
-
 pub mod config;
 pub mod result;
 pub mod session;
