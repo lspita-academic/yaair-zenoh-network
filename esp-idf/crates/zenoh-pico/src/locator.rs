29f133b42425ab46d5bc23a288ca2938c4c3f182
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-04-25 09:29:30 +0200

    locator: use strum for to_string

--- esp-idf/crates/zenoh-pico/src/locator.rs
+++ esp-idf/crates/zenoh-pico/src/locator.rs
@@ -1,19 +1,12 @@
 use std::{fmt::Display, net::SocketAddrV4};
 
-#[derive(Debug, Default)]
+use strum::{Display, EnumString};
+
+#[derive(Debug, Display, EnumString)]
+#[strum(serialize_all = "snake_case")]
 pub enum LocatorProtocol {
-    #[default]
     UDP,
     TCP,
-}
-
-impl Display for LocatorProtocol {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(match self {
-            Self::UDP => "udp",
-            Self::TCP => "tcp",
-        })
-    }
 }
 
 pub struct Locator {
