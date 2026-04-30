61e5a3237b452281a283d521d0c9718f78040a3b
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-04-30 12:22:47 +0200

    zp: sample keyexpr

--- esp-idf/crates/zenoh-pico/src/sample.rs
+++ esp-idf/crates/zenoh-pico/src/sample.rs
@@ -3,15 +3,13 @@ use std::ptr::NonNull;
 use ffi_utils::pointer::NonNullExtensions;
 use zenoh_pico_core::{
     sys::{
-        z_sample_attachment, z_sample_encoding, z_sample_kind, z_sample_kind_t,
-        z_sample_kind_t_Z_SAMPLE_KIND_DELETE, z_sample_kind_t_Z_SAMPLE_KIND_PUT, z_sample_payload,
-        z_sample_timestamp,
+        z_sample_attachment, z_sample_encoding, z_sample_keyexpr, z_sample_kind, z_sample_kind_t, z_sample_kind_t_Z_SAMPLE_KIND_DELETE, z_sample_kind_t_Z_SAMPLE_KIND_PUT, z_sample_payload, z_sample_timestamp
     },
     zvalue::ZValue,
 };
 use zenoh_pico_macros::zwrap;
 
-use crate::{encoding::Encoding, timestamp::Timestamp, zbytes::ZBytes};
+use crate::{encoding::Encoding, keyexpr::KeyExpr, timestamp::Timestamp, zbytes::ZBytes};
 
 #[zwrap(base(name = "sample"), zvalue, zown, ztake)]
 pub struct Sample;
@@ -51,5 +49,9 @@ impl Sample {
 
     pub fn payload(&self) -> &ZBytes {
         ZBytes::from_ptr(unsafe { z_sample_payload(self.zloan()) })
+    }
+
+    pub fn keyexpr(&self) -> &KeyExpr {
+        KeyExpr::from_ptr(unsafe { z_sample_keyexpr(self.zloan()) })
     }
 }
