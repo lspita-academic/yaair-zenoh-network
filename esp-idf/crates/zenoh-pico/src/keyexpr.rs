e2ebbeade365e3ec865f4ba43a57594bfa3ae1b9
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-05-02 09:44:21 +0200

    keyexpr declare

--- esp-idf/crates/zenoh-pico/src/keyexpr.rs
+++ esp-idf/crates/zenoh-pico/src/keyexpr.rs
@@ -1,14 +1,27 @@
 use std::str::FromStr;
 
 use zenoh_pico_core::{
-    result::{IntoZenohResult, ZenohError},
-    sys::{_z_declared_keyexpr_t, z_keyexpr_from_substr_autocanonize},
+    result::{IntoZenohResult, ZenohError, ZenohResult},
+    sys::{_z_declared_keyexpr_t, z_declare_keyexpr, z_keyexpr_from_substr_autocanonize},
     zvalue::{ZOwn, ZValue},
 };
 use zenoh_pico_macros::zwrap;
 
+use crate::session::Session;
+
 #[zwrap(base(name = "keyexpr"), zvalue(value_ty = _z_declared_keyexpr_t), zown)]
 pub struct KeyExpr;
+
+impl KeyExpr {
+    pub fn declare(self, session: &Session) -> ZenohResult<Self> {
+        let mut value = Self::uninitialized();
+        value
+            .inspect_zowned_mut(|z| unsafe {
+                z_declare_keyexpr(session.zloan(), z, self.zloan()).into_zresult()
+            })
+            .map(|_| value)
+    }
+}
 
 impl FromStr for KeyExpr {
     type Err = ZenohError;
