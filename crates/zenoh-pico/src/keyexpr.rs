23b694f57130edf9c0a29c8eb419f8c1b229bd82
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-05-05 08:33:27 +0200

    zp: keyexpr explicit autocanonization + join function

--- crates/zenoh-pico/src/keyexpr.rs
+++ crates/zenoh-pico/src/keyexpr.rs
@@ -1,25 +1,50 @@
 use std::str::FromStr;
 
 use zenoh_pico_macros::zwrap;
-use zenoh_pico_sys::{_z_declared_keyexpr_t, z_keyexpr_equals, z_keyexpr_from_substr_autocanonize};
+use zenoh_pico_sys::{
+    _z_declared_keyexpr_t, z_keyexpr_equals, z_keyexpr_from_substr,
+    z_keyexpr_from_substr_autocanonize, z_keyexpr_join,
+};
 
 use crate::{
-    result::{IntoZenohResult, ZenohError},
+    result::{IntoZenohResult, ZenohError, ZenohResult},
     zvalue::{ZOwn, ZValue},
 };
 
 #[zwrap(base(name = "keyexpr"), zvalue(value_ty = _z_declared_keyexpr_t), zown)]
 pub struct KeyExpr;
 
-impl FromStr for KeyExpr {
-    type Err = ZenohError;
+impl KeyExpr {
+    pub fn new(s: &str) -> ZenohResult<Self> {
+        let mut keyexpr = Self::uninitialized();
+        keyexpr.with_zowned_mut(|z| unsafe {
+            z_keyexpr_from_substr(z, s.as_ptr(), s.len()).into_zresult()
+        })?;
+        Ok(keyexpr)
+    }
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+    pub fn autocanonize(s: &str) -> ZenohResult<Self> {
         let mut keyexpr = Self::uninitialized();
         keyexpr.with_zowned_mut(|z| unsafe {
             z_keyexpr_from_substr_autocanonize(z, s.as_ptr(), &mut s.len()).into_zresult()
         })?;
         Ok(keyexpr)
+    }
+
+    pub fn join_autocanonize(self, other: &KeyExpr) -> ZenohResult<KeyExpr> {
+        let mut keyexpr = Self::uninitialized();
+        keyexpr.with_zowned_mut(|z| unsafe {
+            z_keyexpr_join(z, self.zloan(), other.zloan()).into_zresult()
+        })?;
+        Ok(keyexpr)
+    }
+}
+
+impl FromStr for KeyExpr {
+    type Err = ZenohError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::autocanonize(s)
     }
 }
 
