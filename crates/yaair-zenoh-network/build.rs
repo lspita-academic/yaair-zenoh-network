70a21846fc60ba2c1c8b12fe5cc7c53ca6e9269b
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-05-31 12:02:25 +0200

    fix: use target os instead of feature to choose zenoh pico

---
+++ crates/yaair-zenoh-network/build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+
+    match target_os.as_str() {
+        "espidf" => println!("cargo:rustc-cfg=zenoh-pico"),
+        "none" => panic!("Unsupported target os: none"),
+        _ => {}
+    }
+}
