f3b6af0740e80248d44b107ce51e81f1483ee5e5
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-05-31 12:07:26 +0200

    fix: replace dash with underscore in zenoh pico feature name

--- crates/yaair-zenoh-network/build.rs
+++ crates/yaair-zenoh-network/build.rs
@@ -2,7 +2,7 @@ fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
 
     match target_os.as_str() {
-        "espidf" => println!("cargo:rustc-cfg=zenoh-pico"),
+        "espidf" => println!("cargo:rustc-cfg=zenoh_pico"),
         "none" => panic!("Unsupported target os: none"),
         _ => {}
     }
