f35286f8d9360fb114909862789a6dbb2fea61c7
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-06-06 20:03:14 +0200

    style: multiline comments are not doc comments

--- crates/yaair-zenoh-network/src/zenoh_pico/config.rs
+++ crates/yaair-zenoh-network/src/zenoh_pico/config.rs
@@ -37,10 +37,10 @@ impl From<PeerType> for WhatAmI {
 }
 
 pub struct ZenohConfigBuilderOptions {
-    /// interface is required for multicast, and it's rare that multicast will
-    /// be disabled, so it's not an [Option](Option) type.
-    ///
-    /// <https://github.com/eclipse-zenoh/zenoh-pico#34-basic-pubsub-example---p2p-over-udp-multicast>
+    // interface is required for multicast, and it's rare that multicast will
+    // be disabled, so it's not an [Option](Option) type.
+    //
+    // <https://github.com/eclipse-zenoh/zenoh-pico#34-basic-pubsub-example---p2p-over-udp-multicast>
     pub interface: ConfigString,
 }
 
