5a47f436feb024d449b26d3810c8faef7378fde5
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-06-08 18:14:56 +0200

    feat(zenoh-pico): set build options from env

--- crates/zenoh-pico-sys/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
-    match target_os.as_str() {
-        "espidf" => embuild::espidf::sysenv::output(),
-        _ => {}
-    }
-}
